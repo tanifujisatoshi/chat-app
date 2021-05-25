73e1bc3882367d2b3531602ebf6c9df3d17a68b2
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-05-25 16:28:13 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
+      render :index
     end
   end
 
